a5aa2f93a75744a393e28e0056bdf16f93c95c98
JaneeshaJ2001 <wickramasinghejj.21@uom.lk>
2022-07-15 23:46:43 +0530

    add color.dart file

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  static final Color lightGreen =
+      const Color(0x94AE7E); //opacity 15% for my cart -light green
+  static final Color darkGreen = const Color(
+      0x249079); //opacity : 8% for search bar background //continue shopping- 70%  //publish article-15%
+  static final Color grey = const Color(0xD9D9D9);
+  static final Color brown = const Color(0x9F7B4B);
+  static final Color boldText = const Color(0x1C1C1C);
+  static final Color mediumText = const Color(0x000000);
+  static final Color searchText = const Color(0x303030); //opacity: 30%
+}
