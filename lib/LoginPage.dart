9dc345fd3bf7752ce8e2d6fa569e89c6574b68bc
Thejas0604 <meddepolamact.21@uom.lk>
2022-07-15 23:24:29 +0530

    new

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -11,9 +11,10 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      child: Center(child: Text("Hello")),
-    ));
+    return const Scaffold(
+      body: Center(
+        child: Text("Hello"),
+      ),
+    );
   }
 }
