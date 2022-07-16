726b5774f286aeb660db2558ce19247934096ef4
JaneeshaJ2001 <wickramasinghejj.21@uom.lk>
2022-07-16 14:10:30 +0530

    added home screen header & body files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:agrihome/home_screen_body.dart';
 import 'package:flutter/material.dart';
 import 'bttmnavbar.dart';
 import 'firstScreen.dart';
+import 'home_screen_head.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
-      home: const BtmNavBar(),
+      home: const HomeScreenHead(),
     );
   }
 }
