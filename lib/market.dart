55ac2ed007f4da3e30ee696e73437e08898fc30d
isuranga1 <isuranga6666@gmail.com>
2022-07-16 12:16:50 +0530

    changed

--- lib/market.dart
+++ lib/market.dart
@@ -1,9 +1,11 @@
 import 'homeScreen.dart';
 import 'package:flutter/material.dart';
 
-class Marketplace extends StatefulWidget {
-  const Marketplace({Key? key}) : super(key: key);
+class MarketPlace extends StatelessWidget {
+  const MarketPlace({Key? key}) : super(key: key);
 
   @override
-  State<Marketplace> createState() => _Marketplace State();
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(appbar:Appar()));
+  }
 }
