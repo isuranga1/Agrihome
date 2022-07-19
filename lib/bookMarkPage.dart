5713d9af9566c482a1a73a54e6769a2e049d1b71
Gethz123 <dealwisagt.21@uom.lk>
2022-07-19 12:23:08 +0530

    updated bookMarkPage.dart

--- lib/bookMarkPage.dart
+++ lib/bookMarkPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import'package:agrihome/bookmark.dart';
+
 class BookMarkPage extends StatefulWidget {
   const BookMarkPage({Key? key}) : super(key: key);
 
@@ -19,16 +19,66 @@ class _BookMarkPageState extends State<BookMarkPage> {
       ),
 
       body:
-      Column(
-        children:const [
-          BookMark(verticalGardeningImg : 'assets/imgs/verticalGardening.png',
-            bookmarkHeading: "Vertical Gardening",
-            bookmarkText: "Lorem ipsum dolor sit amet, consectetur adipiscing"
-                " elit. In enim leo, rutrum fermentum feugiat ",),
-          BookMark(verticalGardeningImg: 'assets/imgs/articleImg.png',
-            bookmarkHeading: 'Compost',
-            bookmarkText:'abcde fghr hjryii sbgfjfkfjn',)
-        ],
+      Padding(
+        padding: const EdgeInsets.only(top: 5),
+        child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return Stack(
+              children: [
+                Container(
+                    margin: const EdgeInsets.only(bottom: 15),
+                    height: 120,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color(0XFFE8F5E9)),
+                    child: Padding(
+                      padding:
+                      const EdgeInsets.only(right: 20, left: 140),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children:const  [
+                              Text("Vertical Gardening", style: TextStyle(color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16),
+                              ),
+                              Icon(Icons.bookmark_border,
+                                  size: 15,
+                                  color: Colors.green)
+                            ],
+                          ),
+                          const SizedBox(height: 8),
+                          const Text(" to make compost at home How to make compost at home to make compost at home How to make compost at home",
+                            maxLines: 3,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    width: 120,
+                    height: 110,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image:
+                            AssetImage("assets/imgs/articleImg.png"))),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
 
 
